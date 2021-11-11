485173751e67be597dc4ebac05586e987c71024a
SurendraB12 <89416745+SurendraB12@users.noreply.github.com>
2021-11-11 09:21:37 +0530

    Added modified controller

--- controllers/tshirt.js
+++ controllers/tshirt.js
@@ -30,9 +30,25 @@ exports.Tshirt_detail = function(req, res) {
 }; 
  
 // Handle Tshirt create on POST. 
-exports.Tshirt_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Tshirt create POST'); 
-}; 
+exports.Tshirt_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new Tshirt();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.brand = req.body.brand; 
+    document.material = req.body.material; 
+    document.cost = req.body.cost;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
  
 // Handle Tshirt delete form on DELETE. 
 exports.Tshirt_delete = function(req, res) { 
