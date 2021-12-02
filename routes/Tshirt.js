c09e66cfea373d6adc99f30caec6e64570a73bb1
SurendraB12 <89416745+SurendraB12@users.noreply.github.com>
2021-12-02 11:17:55 +0530

    Addedn

--- routes/Tshirt.js
+++ routes/Tshirt.js
@@ -2,17 +2,32 @@ var express = require('express');
 const Tshirt_controlers = require('../controllers/Tshirt');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on
+//or redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
+
 /* GET costumes */
 router.get('/', Tshirt_controlers.Tshirt_view_all_Page);
 
 router.get('/detail', Tshirt_controlers.Tshirt_view_one_Page);
 
+
 // /* GET create costume page */
-router.get('/create', Tshirt_controlers.Tshirt_create_Page);
+router.get('/create', secured, Tshirt_controlers.Tshirt_create_Page);
 
 
-router.get('/update', Tshirt_controlers.Tshirt_update_Page); 
+router.get('/update', secured, Tshirt_controlers.Tshirt_update_Page); 
 
-router.get('/delete', Tshirt_controlers.Tshirt_delete_Page); 
+router.get('/delete', secured, Tshirt_controlers.Tshirt_delete_Page); 
+/* GET details with id of Tshirt page */
+router.get('/Tshirt/:id', Tshirt_controlers.Tshirt_detail);
+/* DELETE details with id of Tshirt page */
+router.get('/Tshirt/:id', Tshirt_controlers.Tshirt_delete);
 
 module.exports = router;
